3017e2c0dd042b4438d6d2cec439a800e24ceac8
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-09-28 12:13:05 +0200

    Basic game functionalities

---
+++ StroopEffect/src/components/Game.jsx
@@ -0,0 +1,45 @@
+import React, { useRef } from 'react'
+// 
+//   to do
+//    - checking if the answer is correct
+//    - adding 1 points or 0 points
+//    - showing the score
+//    - timer
+//    - user settings
+
+
+const Game = () => {
+
+    const colorPL = ['czarny', "czerwony", "zielony", "żółty", "pomarańczowy", 'niebieski', 'różowy', 'fioletowy']
+    const colorENG = ['black', 'red', 'green', 'yellow', 'orange', 'blue', 'pink', 'purple'];
+
+    const textRef = useRef(null);
+
+    const renderRandomColor = () => {
+        let color;
+        let text;
+
+      color = colorENG[Math.floor(Math.random() * colorENG.length)];
+      text = colorPL[Math.floor(Math.random() * colorPL.length)];
+
+      textRef.current.innerHTML = text;
+      textRef.current.style.color = color;
+
+      console.log(color+" "+text);
+    }
+
+  return (
+    <>
+      <div className='colorTextContainer'>
+        <p className='colorText' ref={textRef}></p>
+      </div>
+      <div className='colorButtonsContainer'>
+        {colorENG.map((color) => (
+          <button key={color} style={{backgroundColor: color}} onClick={renderRandomColor}></button>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Game
