b71ca0173c22f0f77cca65ad0031d421bf1ca785
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-10-04 14:53:40 +0200

    Better Styling for duration form

--- StroopEffect/src/components/Welcome.jsx
+++ StroopEffect/src/components/Welcome.jsx
@@ -15,9 +15,10 @@ const Welcome = ({ setStarted, setTimer, setInitialTimer }) => {
     <div className='welcome_container'>
       <h1>Stroop Game</h1>
       <h4>Press the color that the text is colored with</h4>
-      <p className='duration_info'>How long would you like to play?</p>
-      <input type='number' placeholder="60" className="duration_input" onChange={(e) => timerSetup()}/><span>&nbsp;seconds</span>
-      <br/>
+      <div className='duration_container'>
+        <p className='duration_info'>How long would you like to play?</p>
+        <input type='number' placeholder="60" className="duration_input" onChange={(e) => timerSetup()}/><span>&nbsp;seconds</span>
+      </div>
       <button onClick={() => setStarted((prev) => !prev)}>Start</button>
     </div>
     </>
